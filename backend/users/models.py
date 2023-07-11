b7ee9ae4732bee70b7af657f6a50fb594cc48505
Siktorovich <artem-arhipov-99@mail.ru>
2023-07-11 23:00:39 +0300

    Paste User model in new app

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
-# Create your models here.
+
+
+ROLES = (
+    ('user', 'user'),
+    ('admin', 'admin'),
+)
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField()
+    role = models.CharField(
+        max_length=50,
+        choices=ROLES,
+        default='user',
+        verbose_name='Роль пользователя',
+    )
+    is_subscribed = models.BooleanField(
+        null=True,
+        blank=True,
+        verbose_name='Подписан ли текущий пользователь на этого'
+    )
+
+    class Meta:
+        db_table = 'auth_user'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def save(self, *args, **kwargs):
+        if self.is_superuser:
+            self.role = 'admin'
+        super().save(*args, **kwargs)
+    
+    REQUIRED_FIELDS = [
+        "first_name",
+        "last_name",
+    ]
