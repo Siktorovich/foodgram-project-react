dbf4659078e8e8e6cce0acd3424e060523371ab1
Siktorovich <artem-arhipov-99@mail.ru>
2023-07-13 23:57:41 +0300

    Edit color field in tag serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,25 @@
+import webcolors
+
 from rest_framework import serializers
 from users.serializers import UserSerializer
 from recipes.models import Cart, Favorite, Recipe, Ingredient, Tag
 
 
+class Hex2NameColor(serializers.Field):
+
+    def to_representation(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
+
+
 class TagSerializer(serializers.ModelSerializer):
+    color = Hex2NameColor()
 
     class Meta:
         model = Tag
