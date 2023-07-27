9d15933ba79cc5bed7103431bd545b86b7505b85
Siktorovich <artem-arhipov-99@mail.ru>
2023-07-27 22:06:30 +0300

    Fix debug mode

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'empty')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
